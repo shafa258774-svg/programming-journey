b71c4cc95b583b9dce6b567d8e7172989aee3646
shafa258774-svg <shafa258774@student.smkn1kandeman.sch.id>
2025-10-21 10:59:35 +0700

    Add files via upload

---
+++ Praktik6.8_shafa_xpplg2/Praktik6.8_shafa_xpplg2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Praktik6._8_shafa_xpplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulangPilihan;
+
+            do
+            {
+                // --- Blok Proses akan diulang ---
+                Console.WriteLine("----------------------------------");
+                Console.Write("Masukkan nama anda: ");
+                string nama = Console.ReadLine();
+                Console.WriteLine($"Hallo, {nama}! Proses telah selesai");
+                // --------------------------------------
+                // Pertanyaan untuk mengulang 
+                Console.Write("Apakah Anda ingin mengulang lagi (ya/tidak): ");
+                ulangPilihan = Console.ReadLine().ToLower(); // Mengambil input dan mengubah ke huruf kecil
+
+                Console.WriteLine(); // Baris kosong untuk kerapian
+
+                // Kondisi diperiksa di akhir: Ulangi selam pengguna mengetik "ya"
+            } while (ulangPilihan == "ya");
+              Console.WriteLine("Terima Kasih. Program selesai.");
+
+
+        }
+    }
+}
