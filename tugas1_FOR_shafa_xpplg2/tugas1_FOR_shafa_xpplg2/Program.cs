56b9a7ed451a076e8abd387ff7cd3f7d32017de2
shafa258774-svg <shafa258774@student.smkn1kandeman.sch.id>
2025-10-21 11:01:17 +0700

    Add files via upload

---
+++ tugas1_FOR_shafa_xpplg2/tugas1_FOR_shafa_xpplg2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace tugas1_FOR_shafa_xpplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Menampilkan judul
+            Console.WriteLine("Tabel Perkalian 5:");
+            // Loop for dari 1 hingga 10
+            for (int i = 1; i <= 10; i++)
+            {
+                int hasil = 5 * i;  // Menghitung hasil perkalian
+                Console.WriteLine($"5 x {i} = {hasil}");  // Mencetak hasil
+            }
+        }
+    }
+}
