e79e12aef2416e130db1c06712460e634343af54
shafa258774-svg <shafa258774@student.smkn1kandeman.sch.id>
2025-10-21 09:57:53 +0700

    Add files via upload

---
+++ Praktik6.1_shafa_xpplg2/Praktik6.1_shafa_xpplg2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace contoh_perulangan_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Perulangan untuk menampilkan angka 1 sampai 10");
+
+            // Perulanhan for dimulai di sini
+            for (int i = 1; i <= 10; i++)
+            {
+                // Blok kode yang akan diulang
+                Console.WriteLine("Angka ke-" + i);
+            }
+
+            Console.WriteLine("\nPerulangan selesai.");
+        }
+
+    }
+}
