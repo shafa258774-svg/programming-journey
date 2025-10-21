d882b5e04cf2852df1708bb576a16d67aa216890
shafa258774-svg <shafa258774@student.smkn1kandeman.sch.id>
2025-10-21 14:44:05 +0700

    Add files via upload

---
+++ Tugas3_KonversiSuhu_shafa_xpplg2/Tugas3_KonversiSuhu_shafa_xpplg2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Tugas3_KonversiSuhu_shafa_xpplg2
+{
+    internal class Program
+    {
+        // Fungsi KonversiSuhu: mengubah Celcius ke Fahrenheit
+        static double KonversiSuhu(double celsius)
+        {
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            return fahrenheit;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan suhu dalam Celcius: ");
+            double c = double.Parse(Console.ReadLine());
+
+            // Panggil fungsi KonversiSuhu
+            double hasilKonversi = KonversiSuhu(c);
+
+            Console.WriteLine($"Suhu {c}°C = {hasilKonversi}°F");
+        }
+    }
+}
