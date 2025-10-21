ded7089fc8a8823cb87f49a52a66ebc7de233db2
shafa258774-svg <shafa258774@student.smkn1kandeman.sch.id>
2025-10-21 10:56:22 +0700

    Add files via upload

---
+++ Praktik6.5_shafa_xpplg/Praktik6.5_shafa_xpplg/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Praktik6._5_shafa_xpplg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Inisialisasi variabel penghitung (counter)
+            int angka = 1;
+
+            Console.WriteLine("Menampikan angka 1 sampai 20: ");
+
+            // 2. Blok 'do-while'
+            do
+            {
+                // tampilkan nilai variabel 'angka' saat ini
+                Console.WriteLine(angka);
+
+                // tambahkan nilai 'angka' sebanyak 1 (increment)
+                angka++;
+
+                // uji kondisi: loop berlanjut selama 'angka' kurang dari atau sama dengan 
+
+            } while (angka <= 20);
+            Console.WriteLine("Selesai.");
+        }
+    }
+}
+
