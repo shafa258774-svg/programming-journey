d25394d6101d25e0642755d94864a4b5ae05a2c7
shafa258774-svg <shafa258774@student.smkn1kandeman.sch.id>
2025-10-21 11:02:11 +0700

    Add files via upload

---
+++ tugas2_WHILE_shafa_xpplg2/tugas2_WHILE_shafa_xpplg2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace tugas2_WHILE_shafa_xpplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variabel untuk perulangan hasil penjumlahan
+            int total = 0;  
+            int i = 1;    
+
+            Console.WriteLine("Menghitung jumlah total dari bilangan 1 sampai 5:");
+
+            while (i <= 5)  // Loop while berjalan selama i kurang dari atau sama dengan 5
+            {
+                total += i;  // Tambahkan nilai i ke total
+                i++;       // Increment i untuk melanjutkan loop
+            }
+            // Menampilkan hasil akhir
+            Console.WriteLine("Total : " + total);
+
+        }
+    }
+}
