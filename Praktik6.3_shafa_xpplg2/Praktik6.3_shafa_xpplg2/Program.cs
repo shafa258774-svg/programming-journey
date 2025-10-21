3e7d3cc35b907e75ee5cd9c71850fb07e2a99341
shafa258774-svg <shafa258774@student.smkn1kandeman.sch.id>
2025-10-21 10:54:47 +0700

    Add files via upload

---
+++ Praktik6.3_shafa_xpplg2/Praktik6.3_shafa_xpplg2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Praktik6._3_shafa_xpplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inisialisasi variabel untuk menyimpan input
+            string inputUser = "";
+
+            Console.WriteLine("Program akan terus berjalan hingga Anda mengetik 'keluar'.");
+
+            // Perulangan selama isi variabel inputUser TIDAK SAMA DENGAN "keluar"
+            while (inputUser.ToLower() != "keluar")
+            {
+                Console.WriteLine("\nKetik sesuatu (atau 'keluar' untuk berhenti): ");
+                inputUser = Console.ReadLine(); // Baca input dari user
+
+                Console.WriteLine("Anda mengetik: " + inputUser);
+            }
+            Console.WriteLine("\nProgram selesai. Arigato Gozaimasu ");
+        }
+    }
+}
